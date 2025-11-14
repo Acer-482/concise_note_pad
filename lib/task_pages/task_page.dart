89f7bad00bbabdac1372a8a3b4d914771a25073e
Acer <482936046@qq.com>
2025-11-14 18:23:41 +0800

    optimize allTaskSliverAppbar

--- lib/task_pages/task_page.dart
+++ lib/task_pages/task_page.dart
@@ -18,6 +18,7 @@ class TaskPage extends StatefulWidget {
 
 class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
   late final SlidableController slidableController; // 滑动控制器
+  bool isSortOptionAutoClose = true; // 排序页面自动关闭
 
   @override
   void initState() {
@@ -31,15 +32,19 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
     List<Widget> children,
   ) {
     return showModalBottomSheet(
+      showDragHandle: true,
       context: context,
       builder: (context) => Card(
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          padding: EdgeInsets.all(0),
+          child: ListView(
             children:
                 <Widget>[
-                  Text(title, style: Theme.of(context).textTheme.titleLarge),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                    textAlign: TextAlign.center,
+                  ),
                 ] +
                 children,
           ),
@@ -64,44 +69,75 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
           return Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              SegmentedButton<bool>(
-                segments: [
-                  ButtonSegment(
-                    value: false,
-                    icon: Icon(Icons.arrow_drop_up_rounded),
-                    label: Text('升序'),
-                  ),
-                  ButtonSegment(
-                    value: true,
-                    icon: Icon(Icons.arrow_drop_down_rounded),
-                    label: Text('降序'),
-                  ),
-                ],
-                selected: {taskManager.isReverseSort},
-                onSelectionChanged: (Set<bool> newSelection) {
-                  setState(() {
-                    taskManager.isReverseSort = newSelection.first;
-                    taskManager.update(); // 更新任务管理器
-                  });
-                },
-                showSelectedIcon: false,
-              ), // 升序降序选择
+              Padding(
+                padding: EdgeInsetsGeometry.fromLTRB(20, 4, 20, 4),
+                child: Column(
+                  spacing: 10, // 间距
+                  children: [
+                    Row(
+                      children: [
+                        Text('排序方式：'),
+                        SegmentedButton<bool>(
+                          segments: [
+                            ButtonSegment(
+                              value: false,
+                              icon: Icon(Icons.arrow_drop_up_rounded),
+                              label: Text('升序'),
+                            ),
+                            ButtonSegment(
+                              value: true,
+                              icon: Icon(Icons.arrow_drop_down_rounded),
+                              label: Text('降序'),
+                            ),
+                          ],
+                          selected: {taskManager.isReverseSort},
+                          onSelectionChanged: (Set<bool> newSelection) {
+                            setState(() {
+                              taskManager.isReverseSort = newSelection.first;
+                              taskManager.update(); // 更新任务管理器
+                            });
+                          },
+                          showSelectedIcon: false,
+                        ),
+                      ],
+                    ), // 排序方式选择
+                    Row(
+                      children: [
+                        Text('排序完成项置于底层：'),
+                        Checkbox(value: false, onChanged: (v) {}),
+                      ],
+                    ), // 完成项目置底
+                    Row(
+                      children: [
+                        Text('选择类型后自动关闭当前页'),
+                        Checkbox(
+                          value: isSortOptionAutoClose,
+                          onChanged: (v) =>
+                              setState(() => isSortOptionAutoClose = v!),
+                        ),
+                      ],
+                    ), // 自动关闭
+                  ],
+                ),
+              ),
               RadioGroup<SortOption>(
                 groupValue: taskManager.sortOption,
                 onChanged: (SortOption? value) {
                   if (value != null) {
+                    // 更新状态 //
                     setState(() {
-                      taskManager.sortOption = value;
-                      taskManager.update();
+                      taskManager.sortOption = value; // 设置选项
+                      taskManager.update(); // 更新
                     });
-                    // 延迟关闭
-                    Timer(Duration(milliseconds: 300), () {
-                      if (mounted) Navigator.of(context).pop();
-                    });
+                    // 自动延迟关闭 //
+                    if (isSortOptionAutoClose) {
+                      Timer(Duration(milliseconds: 300), () {
+                        if (mounted) Navigator.of(context).pop();
+                      });
+                    }
                   }
                 },
                 child: Column(
-                  mainAxisSize: MainAxisSize.min,
                   children: [
                     RadioListTile<SortOption>(
                       value: SortOption.importance,
@@ -156,24 +192,17 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
           tooltip: '过滤器',
           icon: const Icon(Icons.filter_list),
         ),
-        PopupMenuButton(
-          tooltip: '更多设置',
-          itemBuilder: (context) => [
-            PopupMenuItem(
-              child: ListTile(leading: Icon(Icons.sort), title: Text('排序方式')),
-              onTap: () {
-                _showSortOption();
-              },
-            ),
-            PopupMenuItem(
-              child: ListTile(
-                leading: Icon(Icons.view_list),
-                title: Text('切换视图'),
-              ),
-              onTap: () {
-                _showViewOption();
-              },
-            ),
+        IconButton(
+          onPressed: () => _showSortOption(),
+          tooltip: '排序方式',
+          icon: const Icon(Icons.sort_rounded),
+        ),
+        IconButton(
+          onPressed: () => _showViewOption(),
+          tooltip: '切换视图',
+          icon: Icon(Icons.view_list),
+        ),
+        PopupMenuButton(tooltip: '更多设置', itemBuilder: (context) => [
           ],
         ),
       ], // 选项
