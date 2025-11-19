e697b00ddfd93449d5c778f4c1c6a55b3d3d7a17
Acer <482936046@qq.com>
2025-11-19 09:22:30 +0800

    Created content about the page Added package package_info_plus

--- lib/page/about_page.dart
+++ lib/page/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -9,13 +10,63 @@ class AboutPage extends StatefulWidget {
 
 class _AboutPageState extends State<AboutPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('关于')),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: null, //
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 40,
+            children: [
+              _buildAboutContext(),
+              ElevatedButton(
+                onPressed: () => showLicensePage(context: context),
+                child: Text('许可证信息'),
+              ),
+            ],
+          ),
+        ),
       ),
     ); // 骨架
+  }
+
+  Widget _buildAboutContext() {
+    return FutureBuilder<PackageInfo>(
+      future: PackageInfo.fromPlatform(), // 获取平台信息
+      builder: (context, snapshot) {
+        // 获取到数据 构建页面
+        if (snapshot.connectionState == ConnectionState.done) {
+          PackageInfo packageInfo = snapshot.data!;
+          return Column(
+            spacing: 10,
+            children: [
+              CircleAvatar(radius: 32,child: Icon(Icons.apps, size: 24,),),
+              Text('简记', style: Theme.of(context).textTheme.displayLarge),
+              Text(
+                '© 2024 Acer',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                'Version ${packageInfo.version} (${packageInfo.buildNumber})',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          // 正在获取 显示等待加载指示器
+          return const CircularProgressIndicator();
+        } else {
+          return const Text('获取应用信息失败');
+        }
+      },
+    );
   }
 }
