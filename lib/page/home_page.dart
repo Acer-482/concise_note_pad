1d02bab862c4eddf20a9ba1bc3ac47d6daae9f0b
Acer <482936046@qq.com>
2025-11-19 23:28:21 +0800

    Added native AnimationController to the HomePage

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_icons_animated/flutter_animated_icons.dart';
+// import 'package:flutter_icons_animated/icons8.dart';
+// import 'package:lottie/lottie.dart';
 
 /// 主页面
 ///
@@ -10,12 +13,27 @@ class HomePage extends StatefulWidget {
   State<StatefulWidget> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller; // 动画控制器
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    ); // 创建动画控制
+    _controller.forward(); // 开始播放
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // color: Colors.white,
-      // child: Lottie.asset(Icons8.book)
-    );
+    return Center();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
